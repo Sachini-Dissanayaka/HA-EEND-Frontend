e5445e764968608c7084c55ee39c5eab8f9cdbe6
Sachini <dmsachiniacc@gmail.com>
2022-04-28 13:49:39 +0530

    init react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+import Home from "./screens/Home";
+
+import "./App.css";
+import "antd/dist/antd.css";
+
+const App: FC = () => {
+  return <Home />;
+};
+
+export default App;
